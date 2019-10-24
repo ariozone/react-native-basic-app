0684ecc3ddb9e72725066147734c1ffb9879cc9c
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-23 18:56:48 -0700

    Added a react-native button component to the home screen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
 import React from "react"
-import { Text, StyleSheet } from "react-native"
+import { Text, StyleSheet, View, Button } from "react-native"
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World!</Text>
+  return (
+    <View>
+      <Text style={styles.text}>Hello World!</Text>
+      <Button
+        title='Go to Components Demo'
+        onPress={() => {
+          console.log("pressed!")
+        }}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
