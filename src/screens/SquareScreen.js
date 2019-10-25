07e303196f4f5e209831c48d87610b8035aa8bbc
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-25 16:52:58 -0700

    Enhanced ColorCounter to keep color numbers in valid boundries.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,26 +3,34 @@ import { View, Text, Button, StyleSheet } from "react-native"
 import ColorCounter from "../components/ColorCounter"
 
 const SquareScreen = () => {
-  const [red, setRed] = useState(100)
-  const [green, setGreen] = useState(100)
-  const [blue, setBlue] = useState(100)
+  const [red, setRed] = useState(0)
+  const [green, setGreen] = useState(0)
+  const [blue, setBlue] = useState(0)
+
+  const CHANGE_AMOUNT = 15
+  const setColor = (color, change) => {
+    if (color + change > 255 || color + change < 0) return
+    else {
+      setRed(red + change)
+    }
+  }
 
   return (
     <View>
       <ColorCounter
         color='Red'
-        onIncrease={() => setRed(red + 10)}
-        onDecrease={() => setRed(red - 10)}
+        onIncrease={() => setColor(red, CHANGE_AMOUNT)}
+        onDecrease={() => setColor(red, -1 * CHANGE_AMOUNT)}
       ></ColorCounter>
       <ColorCounter
         color='Green'
-        onIncrease={() => setGreen(green + 10)}
-        onDecrease={() => setGreen(green - 10)}
+        onIncrease={() => setGreen(green < 256 && green + CHANGE_AMOUNT)}
+        onDecrease={() => setGreen(green >= 0 && green - CHANGE_AMOUNT)}
       ></ColorCounter>
       <ColorCounter
         color='Blue'
-        onIncrease={() => setBlue(blue + 10)}
-        onDecrease={() => setBlue(blue - 10)}
+        onIncrease={() => setBlue(blue < 256 && blue + CHANGE_AMOUNT)}
+        onDecrease={() => setBlue(blue >= 0 && blue - CHANGE_AMOUNT)}
       ></ColorCounter>
       <View
         style={{
