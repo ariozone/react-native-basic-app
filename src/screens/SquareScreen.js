35d285c7da252dbd040e248f54a19fb78a7639dc
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-28 18:05:53 -0700

    Applied the state with the created reducer.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,37 +3,44 @@ import { View, Text, Button, StyleSheet } from "react-native"
 import ColorCounter from "../components/ColorCounter"
 
 const reducer = (state, action) => {
-  switch(action.color) {
-    case 'red':
-      return {...state, red: state.red + action.amount}
-    case 'green':
-      return {...state, green: state.green + action.amount}
-    case 'blue':
-      return {...state, blue: state.blue + action.amount}
-      default:
-        return state
+  switch (action.color) {
+    case "red":
+      return { ...state, red: state.red + action.amount }
+    case "green":
+      return { ...state, green: state.green + action.amount }
+    case "blue":
+      return { ...state, blue: state.blue + action.amount }
+    default:
+      return state
   }
 }
 const SquareScreen = () => {
-const [state, despatch] = useReducer(reducer, {red: 0, green: 0, blue: 0})
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
   const CHANGE_AMOUNT = 15
+  const { red, green, blue } = state
 
   return (
     <View>
       <ColorCounter
         color='Red'
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ amount: CHANGE_AMOUNT, color: "red" })}
+        onDecrease={() =>
+          dispatch({ amount: -1 * CHANGE_AMOUNT, color: "red" })
+        }
       ></ColorCounter>
       <ColorCounter
         color='Green'
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ amount: CHANGE_AMOUNT, color: "green" })}
+        onDecrease={() =>
+          dispatch({ amount: -1 * CHANGE_AMOUNT, color: "green" })
+        }
       ></ColorCounter>
       <ColorCounter
         color='Blue'
-        onIncrease={() => }
-        onDecrease={() =>  }
+        onIncrease={() => dispatch({ amount: CHANGE_AMOUNT, color: "blue" })}
+        onDecrease={() =>
+          dispatch({ amount: -1 * CHANGE_AMOUNT, color: "blue" })
+        }
       ></ColorCounter>
       <View
         style={{
