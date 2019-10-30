9734d2fcab51a8ce558696bfe463867c3c105551
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-29 19:00:26 -0700

    Implemented text input that updates the state on every change.

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, Text, StyleSheet, TextInput } from "react-native"
 
 const TextScreen = () => {
+  const [text, setText] = useState("")
   return (
     <View>
-      <TextInput style={styles.input}></TextInput>
+      <Text style={styles.text}>Enter Name: </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={text}
+        onChangeText={input => setText(input)}
+      ></TextInput>
+      <Text style={styles.text}>My name is {text}</Text>
     </View>
   )
 }
 const styles = StyleSheet.create({
   input: {
     margin: 15,
-    borderColor: "black",
+    borderColor: "gray",
+    color: "gray",
     borderWidth: 1,
-    height: 50
-  }
+    height: 50,
+    fontSize: 30
+  },
+  text: { fontSize: 30 }
 })
 export default TextScreen
