34601165996ef230a77b08a4b561b332746d313b
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-30 19:52:08 -0700

    Applied all layout systems in react native to present styled elements.

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { View, StyleSheet, Text } from "react-native"
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Child #1</Text>
-      <Text style={styles.textTwoStyle}>Child #2</Text>
-      <Text style={styles.textThreeStyle}>Child #3</Text>
+      <View style={styles.viewOneStyle}></View>
+      <View style={styles.viewTwoStyle}></View>
+      <View style={styles.viewThreeStyle}></View>
     </View>
   )
 }
@@ -15,25 +15,12 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 5,
     borderColor: "black",
-
+    flexDirection: "row",
+    justifyContent: "space-between",
     height: 200
   },
-  textOneStyle: {
-    borderWidth: 5,
-    borderColor: "red"
-  },
-  textTwoStyle: {
-    borderWidth: 5,
-    borderColor: "red",
-    position: "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
-  },
-  textThreeStyle: {
-    borderWidth: 5,
-    borderColor: "red"
-  }
+  viewOneStyle: { width: 50, height: 50, backgroundColor: "red" },
+  viewTwoStyle: { width: 50, height: 50, backgroundColor: "green", top: 50 },
+  viewThreeStyle: { width: 50, height: 50, backgroundColor: "purple" }
 })
 export default BoxScreen
